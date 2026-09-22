42e870dceb3f22c7e03831729b4b4cc523153b6c
aleezazahra <azahradum@gmail.com>
2026-09-22 02:35:40 -0700

    created the player

--- shooter.py
+++ shooter.py
@@ -7,19 +7,25 @@ SCREEN_HEIGHT=int(SCREEN_WIDTH*0.8)
 
 screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Shooter game")
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self,x,y,scale,):
+        pygame.sprite.Sprite.__init__(self)
+        img=pygame.image.load('img/player/Idle/0.png')
+     
+        self.image=pygame.transform.scale(img,(int(img.get_width()*scale),int(img.get_height()*scale)))
+        self.rect=self.image.get_rect()  #used for contolling positions and stuff
+        self.rect.center=(x,y)
+    def draw(self):
+        screen.blit(self.image,self.rect)
 
-x=200
-y=200
-img=pygame.image.load('img/player/Idle/0.png')
+player=Soldier(200,200,3)
 
 
-rect=img.get_rect()  #used for contolling positions and stuff
-rect.center=(x,y)
 
 run=True
 while run:
 
-    screen.blit(img,rect)
+    player.draw()
 
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
